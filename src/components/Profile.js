a40bc874f54b734e29811d9bfdf65ba68c584890
JesusRestrepo <jesusrestrepo2003@gmail.com>
2021-09-06 09:49:50 -0500

    V1

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,6 +3,7 @@ import { Button } from 'reactstrap';
 
 import img from './img.png';
 import img2 from './img2.png';
+import img3 from './img3.png';
 
 const Profile = ()=> {
     return(
@@ -103,6 +104,17 @@ const Profile = ()=> {
                                     <p className="card-text">Pequeño juego infantil en el cual, se juega contra la computadora, tiene 3 opciones, piedra, papel o tijera, de acuerdo 
                                     a lo que escojas y escoja la com, te dice si ganaste o perdiste.</p>
                                     <Button href="https://jesusrestrepo.github.io/game.github.io/" color="primary" target="_blank" className="button">Ir al proyecto</Button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col7">
+                            <div className="card">
+                                <img src={img3} className="card-img-top" alt="..."/>
+                                <div className="card-body">
+                                    <h5 className="card-title">Sitio web de ventas online (en construcción).</h5>
+                                    <p className="card-text">Pequeño sitio web de una pequeña compañía para vender productos como gadgets tecnólogicos
+                                    por medio online, con medio de pago, etc.</p>
+                                    <Button href="https://jesusrestrepo.github.io/shop/" color="primary" target="_blank" className="button">Ir al proyecto</Button>
                                 </div>
                             </div>
                         </div>
